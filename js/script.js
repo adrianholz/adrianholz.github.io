e2695dfc83a21948a75f4e5d7c85b9d73ca551b9
Adrian Holzschuh <adrian.holzschuh@gmail.com>
2022-06-23 15:27:09 -0300

    Scroll Animation

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ const occupation_card = document.querySelector(".occupation-inner");
 const occupation_front = document.querySelector(".occupation-front");
 const occupation_back = document.querySelector(".occupation-back");
 const splash = document.querySelector(".splash");
+const sections = document.querySelectorAll(
+  "section > div:nth-child(1), .contact .contact-inner"
+);
+const halve = window.innerHeight * 0.4;
 
 occupation_front.addEventListener("click", function handleClick() {
   occupation_card.classList.add("card-rotation");
@@ -14,7 +18,6 @@ occupation_back.addEventListener("click", function handleClick() {
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
-
     document.querySelector(this.getAttribute("href")).scrollIntoView({
       behavior: "smooth",
     });
@@ -26,3 +29,17 @@ document.addEventListener("DOMContentLoaded", (e) => {
     splash.classList.add("vanish");
   }, 1700);
 });
+
+function scrollAnim() {
+  sections.forEach((item) => {
+    const sectionTop = item.getBoundingClientRect().top - halve;
+    if (sectionTop < 0) {
+      item.classList.add("active");
+    } else {
+      item.classList.remove("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", scrollAnim);
+scrollAnim();
