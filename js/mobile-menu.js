c51f305b876c267073dfc7e910b4572421b21f45
Adrian Holzschuh <adrian.holzschuh@gmail.com>
2022-08-17 12:20:41 -0300

    Mobile optimizations

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,6 +1,8 @@
 export default function mobileMenu() {
   const menuButton = document.querySelector(".menu-mobile");
   const menuMobile = document.querySelector("nav");
+  const menuLi = document.querySelectorAll("nav ul li");
+  const menuLink = document.querySelectorAll("nav ul li a");
 
   function showMenu() {
     menuMobile.classList.toggle("active");
@@ -8,4 +10,20 @@ export default function mobileMenu() {
   }
 
   menuButton.addEventListener("click", showMenu);
+  document.documentElement.addEventListener("click", (event) => {
+    if (!menuButton.contains(event.target)) {
+      menuMobile.classList.remove("active");
+      menuButton.classList.remove("active");
+    }
+  });
+
+  menuLi.forEach((item, index) => {
+    item.addEventListener("click", () => {
+      document
+        .querySelector(menuLink[index].getAttribute("href"))
+        .scrollIntoView({
+          behavior: "smooth",
+        });
+    });
+  });
 }
