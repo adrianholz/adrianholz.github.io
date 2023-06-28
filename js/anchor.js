5d7baa29db7c5af56e18424252d63ade7443a27b
Adrian Holzschuh <adrian.holzschuh@gmail.com>
2023-06-28 15:35:09 -0300

    Update anchor.js

--- js/anchor.js
+++ js/anchor.js
@@ -2,7 +2,6 @@ export default function anchor() {
   document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     anchor.addEventListener("click", function (e) {
       e.preventDefault();
-      console.log(document.querySelector(this.getAttribute("href")));
       document.querySelector(this.getAttribute("href")).scrollIntoView({
         behavior: "smooth",
       });
